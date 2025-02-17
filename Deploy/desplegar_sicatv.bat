701539d0b4414fca5b7f9b3ad829e5a2a51a781c
Neocani <111393667+Neocani@users.noreply.github.com>
2025-02-16 22:43:04 -0500

    Commit

--- Deploy/desplegar_sicatv.bat
+++ Deploy/desplegar_sicatv.bat
@@ -4,4 +4,4 @@ move .\CloudAppPublisher .\SICATV_v2022R3\CloudAppPublisher
 
 copy .\cloudapplauncher.js /Y .\SICATV_v2022R3\js\cloudapplauncher.js
 
-swa deploy .\SICATV_v2022R3 --env Production --verbose -d ba5f1d9996c464e4f26a13055cc556eab58285c53974a6872ee1b113cbec287304-10177840-35cb-49dd-9476-24d5a2585b0200f06270127dd30f
+swa deploy --verbose=silly .\SICATV_v2022R3 --env Production -d ba5f1d9996c464e4f26a13055cc556eab58285c53974a6872ee1b113cbec287304-10177840-35cb-49dd-9476-24d5a2585b0200f06270127dd30f
